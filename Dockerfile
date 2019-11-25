a941c1d6be8265405620933366fa6af19f767ffd
Damian Sloane <dddaaammmooo@hotmail.com>
2019-11-26 10:59:09 +1100

    Cleanup

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM alpine:3.10
 
-RUN apk add --no-cache bash gawk sed grep bc coreutils curl
+RUN apk add --no-cache bash curl
 
 COPY entrypoint.sh /bin
 ENTRYPOINT ["/bin/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -8,23 +8,23 @@ OPSGENIE_API_KEY=${4}
 
 # Make sure a message was defined
 if [[ -z "${MESSAGE}" ]]; then
-    echo "ERROR: No alert message was set"
+    echo "ERROR: No alert message was set while attempting to generate OpsGenie alert"
     exit 1;
 fi
 
 # Make sure an alias was defined
 if [[ -z "${ALIAS}" ]]; then
-    echo "ERROR: No alias was set"
+    echo "ERROR: No alert alias was set while attempting to generate OpsGenie alert"
     exit 2;
 fi
 
 # Make sure an acceptable priority level was defined
 if [[ "P1" != "${PRIORITY}" ]] && [[ "P2" != "${PRIORITY}" ]] && [[ "P3" != "${PRIORITY}" ]] && [[ "P4" != "${PRIORITY}" ]] && [[ "P5" != "${PRIORITY}" ]]; then
-    echo "ERROR: An invalid priority level (${PRIORITY}) was set, it must be one of the valid OpsGenie alert levels (P1-P5)"
+    echo "ERROR: An invalid alert priority level (${PRIORITY}) was set, it must be one of the valid OpsGenie alert levels (P1-P5)"
     exit 3;
 fi
 
-# Send alert to OpsGenie API
+# Send alert via curl request to OpsGenie API
 curl -X POST https://api.opsgenie.com/v2/alerts \
     -H "Host: api.opsgenie.com" \
     -H "Authorization: Basic ${OPSGENIE_API_KEY}" \
